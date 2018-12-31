531e0381b9d4faad02dd8c21fc3907a1cfef7f33
lyes kherbiche <kerbiche@gmail.com>
2018-12-31 03:30:02 +0100

    display area

---
+++ server/src/main/java/am/zgh/snakegame/server/GameFrame.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2018 Lyes Kherbiche
+ * <kerbiche@gmail.com>
+ */
+package am.zgh.snakegame.server;
+
+import javax.swing.JFrame;
+
+/**
+ * The <code>GameFrame</code> class represents the container in witch to display
+ * the game.
+ * 
+ * @author L KHERBICHE
+ * @since 0.0.1-RELEASE
+ */
+@SuppressWarnings("serial")
+public class GameFrame extends JFrame {
+
+}
