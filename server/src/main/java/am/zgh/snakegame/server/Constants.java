bf39ab6696677d68ade9d1a469b48aa2d61e3797
lyes kherbiche <kerbiche@gmail.com>
2019-01-01 23:14:49 +0100

    interf const

---
+++ server/src/main/java/am/zgh/snakegame/server/Constants.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2018-2019 snakegame_Lyes_Kherbiche. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * <kerbiche@gmail.com>
+ */
+package am.zgh.snakegame.server;
+
+/**
+ * The <code>Constants</code> inteface represents
+ * 
+ * @author L KHERBICHE
+ * @since 0.0.1-RELEASE
+ */
+public interface Constants {
+
+	public final static int NBR_OF_COL = 30;
+	public final static int NBR_OF_LIGN = 30;
+	public final static int PIX_SQUARE = 15;
+
+}
