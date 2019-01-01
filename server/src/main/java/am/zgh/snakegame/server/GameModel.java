a605ada5a7068447aaa588df630a6d417db532e4
lyes kherbiche <kerbiche@gmail.com>
2019-01-01 02:59:00 +0100

    snake game model

---
+++ server/src/main/java/am/zgh/snakegame/server/GameModel.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2018 Lyes Kherbiche
+ * <kerbiche@gmail.com>
+ */
+package am.zgh.snakegame.server;
+
+import java.awt.Graphics;
+
+/**
+ * The <code>GameModel</code> class represents
+ * 
+ * @author L KHERBICHE
+ * @since 0.0.1-RELEASE
+ */
+public class GameModel {
+
+	public void calculate() {
+	}
+
+	public void display(Graphics g) {
+	}
+}
